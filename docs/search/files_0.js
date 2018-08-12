8038eb7d8741f43be1e5cc6ed78bc5fee0f00348
Ryan Moore <moorer@udel.edu>
2018-08-12 12:33:19 -0400

    Remove cmake headers from docs

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['rya_2ecmake_2eh',['rya.cmake.h',['../rya_8cmake_8h.html',1,'']]],
   ['rya_2eh',['rya.h',['../rya_8h.html',1,'']]],
   ['rya_5ffile_2ec',['rya_file.c',['../rya__file_8c.html',1,'']]],
   ['rya_5ffile_2eh',['rya_file.h',['../rya__file_8h.html',1,'']]],
